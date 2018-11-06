ae7a025a7fe25a4bb1071560a1d8ad5402c0073a
Naman Manchanda <naman.mohan09@outlook.com>
2018-11-06 22:53:06 +0530

    changes made

--- hello_world.rb
+++ hello_world.rb
@@ -9,3 +9,20 @@ p b
 puts a.include?("Nick")
 p b.shuffle!
 
+# Appending in an array
+a<<10
+a.push(11)
+p a
+
+# Adding element at start of the array
+a.unshift("Nice")
+p a
+
+# Removing last element from array
+a.pop
+p a
+
+# Removing duplicates from an array
+c = [1,1,2,2,3,3,4]
+c.uniq!
+p c
