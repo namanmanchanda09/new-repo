1fcd0969a7fd6653b285514573444a82e4f36cec
Naman Manchanda <naman.mohan09@outlook.com>
2018-11-07 00:02:54 +0530

    changes made in hash

--- hash.rb
+++ hash.rb
@@ -1,3 +1,19 @@
 # Hash
 details = {'name' => 'naman', 'color' => 'red'}
 puts details
+puts details["color"]
+
+
+d = {a:1 , b:2 , c:3 , d:4}
+puts d
+puts d[:c]
+
+# Adding elements in hash
+d[:e] = 7
+puts d
+
+# Delete element
+d.delete(:b)
+puts d
+
+
