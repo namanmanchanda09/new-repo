f1c9030b16c59aa98c9161e12ca631dbfe75f380
Naman Manchanda <naman.mohan09@outlook.com>
2018-11-07 23:06:37 +0530

    changes made in hash.rb

--- hash.rb
+++ hash.rb
@@ -16,4 +16,12 @@ puts d
 d.delete(:b)
 puts d
 
+# Iteration in a hash
+new_hash = {a:1, b:2, c:3, d:4, e:5, f:6}
+new_hash.each {|i,j| new_hash.delete(i) if j>3}
+puts new_hash
+
+# Selecting only odd elements
+p = {a:1 , b:2 , c:3 , d:4 , e:5 , f:6 ,}
+puts p.select {|i,j| j.odd?}
 
