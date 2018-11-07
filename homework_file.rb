bbca684e7d75cfd115dfb49609c47c98572acd9b
Naman Manchanda <naman.mohan09@outlook.com>
2018-11-07 23:44:50 +0530

    added homework_file.rb

---
+++ homework_file.rb
@@ -0,0 +1,31 @@
+book = {
+    "newyork" => "212",
+    "newjersey" => "345",
+    "ohio" => "112",
+    "texas" => "978",
+    "SanFrancisco" => "678",
+    "sanjose" => "354"
+}
+
+def area(book,name)
+  puts book[name]
+end
+
+loop do
+  puts "Do you want to lookup a city (Y/N)"
+  answer = gets.chomp
+  if answer=="Y"
+    puts "Enter city name"
+    city = gets.chomp
+    puts area(book,city)
+  else
+    break
+  end
+end
+
+
+
+
+
+
+
